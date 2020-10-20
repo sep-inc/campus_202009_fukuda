d4d2c0a25f86480baf84f70498b2555d9e796919
Huku1225 <fksoccer1225@icloud.com>
2020-10-20 14:15:58 +0900

    RunGame.cpp/.hを追加

---
+++ RunGame/RunGame/Src/RunGame/RunGame.cpp
@@ -0,0 +1,65 @@
+﻿#include "RunGame.h"
+
+RunGame* RunGame::p_instance = 0;
+
+RunGame::RunGame():
+	m_step(RunGameStep::STEP_INITIALIZE),
+	m_result(RunGameResult::NONE),
+	m_is_game_finish(false)
+{
+
+}
+
+RunGame::~RunGame()
+{
+	DestroyObjects();
+}
+
+RunGame* RunGame::Instance()
+{
+	if (p_instance == 0) {
+		p_instance = new RunGame;
+	}
+	return p_instance;
+}
+
+void RunGame::Update()
+{
+	switch (m_step) {
+	case RunGameStep::STEP_INITIALIZE:
+		/* オブジェクトの生成 */
+		CreateObjects();
+
+		/* 初期化処理 */
+
+		/* ステップ移行 */
+		m_step = RunGameStep::STEP_UPDATE;
+		break;
+
+	case RunGameStep::STEP_UPDATE:
+		/* 更新処理 */
+
+		break;
+
+	case RunGameStep::STEP_END:
+		m_is_game_finish = true;
+		DestroyObjects();
+		break;
+	}
+}
+
+void RunGame::Draw()
+{
+}
+
+void RunGame::SetResult()
+{
+}
+
+void RunGame::CreateObjects()
+{
+}
+
+void RunGame::DestroyObjects()
+{
+}
---
+++ RunGame/RunGame/Src/RunGame/RunGame.h
@@ -0,0 +1,84 @@
+﻿#ifndef RUNGAME_H_
+#define RUNGAME_H_
+
+/**
+* @file RunGame.h
+* @brief RunGameクラスのヘッダ
+*/
+
+/**
+* @class RunGame
+* @brief Runゲーム本体
+*/
+class RunGame
+{
+public:
+	static RunGame* Instance();
+
+	/**
+	* @breif ゲーム本体の更新関数
+	*/
+	void Update();
+
+	/**
+	* @brief 描画処理関数
+	*/
+	void Draw();
+
+	/**
+	* @brief 勝敗情報のセット関数
+	*/
+	void SetResult();
+
+	/**
+	* @brief ゲーム終了判定関数
+	* @return bool ゲーム終了ならtrue
+	*/
+	inline bool IsGameFinish()const { return m_is_game_finish; }
+
+
+public:
+	//! 各オブジェクトのポインタ変数群
+
+
+private:
+	RunGame();
+	~RunGame();
+
+	/**
+	* @brief オブジェクト生成関数
+	*/
+	void CreateObjects();
+
+	/**
+	* @brief オブジェクト破棄関数
+	*/
+	void DestroyObjects();
+
+	static RunGame* p_instance;
+
+
+private:
+	//! 処理ステップの種類
+	enum class RunGameStep : int {
+		STEP_INITIALIZE,	//! 初期化ステップ
+		STEP_UPDATE,		//! 更新ステップ
+		STEP_END			//! 終了ステップ
+	};
+
+	//! 勝敗の種類
+	enum class RunGameResult : int {
+		NONE,	//! なし
+		WIN,	//! 勝利
+		LOSE	//! 敗北
+	};
+
+	RunGameStep m_step;		//! 現在のステップ
+
+	RunGameResult m_result;	//! 勝敗状況
+	bool m_is_game_finish;	//! ゲーム終了フラグ
+
+};
+
+#endif
+
